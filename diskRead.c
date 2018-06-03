d2a1e3ca351bebc9578c6a8e3237b890cd1011ff
root <root@localhost.localdomain>
2018-06-03 08:49:11 +0800

    完善了对系统分区表的读取，可以读取出所有系统分区的位置。

--- diskRead.c
+++ diskRead.c
@@ -2,42 +2,61 @@
 
 int main(int argc, char* argv[]){
   char buf_r[BUFFER_SIZE]; //读取数据的缓冲区大小
-  char *dev = "/dev/sda";
-  int offset = 0;
-
+  char *dev = "/dev/sdb";
   FILE *fp; // 文件描述符
 
+  GPT_HEADER g_header;
+
   fp = fopen(dev,"r"); 
+
   if (fp == NULL){
     fprintf(stderr, "open %s failed %s\n",dev,fp);
     return (-1);
   }
-  offset = find_gpt_header_signature(fp);
-  
-  gpt_header g_header;
-  // 将文件指针放置在512
-  fseek(fp,offset,SEEK_SET);
-  print_gpt_header(fp,&g_header);
 
-  fprintf(stdout,"%d",offset);
-  putchar('\n');
+  //读取分区表头
+  read_gpt_header(fp,&g_header);
+  //output_gpt_header(&g_header);
 
+  //读取分区表
+  PARTITION partitions[128] = {0};
+  read_partition_info(fp, &g_header, partitions);
+  //output_partition(partitions);
+
+  read_first_partition(fp,partitions);
   fclose(fp);
   return 0;
 }
 
-int print_gpt_header(FILE* fp,gpt_header *g_header){
-  memset(g_header,0,sizeof(gpt_header)); // 将heaher中的值全部值为0
+int read_gpt_header(FILE* fp,GPT_HEADER *g_header){
 
-  if (!fread(g_header,sizeof(gpt_header),1,fp)) {
+  int offset = find_gpt_header_signature(fp);
+  // 重定位文件指针至GPT头的起始位置
+  fseek(fp,offset,SEEK_SET);
+  memset(g_header,0,sizeof(GPT_HEADER)); // 将heaher中的值全部置为0
+
+  if (!fread(g_header,sizeof(GPT_HEADER),1,fp)) {
     fprintf(stderr,"get gpt header wrong %d",fp);
+    return 0;
   }
-  fprintf(stdout,"version is = %d\n",g_header->version);
-  fprintf(stdout,"header_size is = %d\n",g_header->header_size);
-  return 0;
+  return 1;
 }
 
-void format_output(char* data,int size){
+void output_gpt_header(GPT_HEADER *g_header){
+  
+  fprintf(stdout,"signature is: 0x%llx\n",g_header->signature);
+  fprintf(stdout,"version is: %d\n",g_header->version);
+  fprintf(stdout,"header_size is: %d\n",g_header->header_size);
+  fprintf(stdout,"efi_header_start: %llu\n",g_header->efi_header_start);
+  fprintf(stdout,"efi_header_backup: %llu\n",g_header->efi_header_backup);
+  fprintf(stdout,"gpt_partition_start: %llu\n",g_header->gpt_partition_start);
+  fprintf(stdout,"gpt_partition_end: %llu\n",g_header->gpt_partition_end);
+  fprintf(stdout,"gpt start is: %d\n",g_header->partition_start);
+  fprintf(stdout,"gpt entry max: %d\n",g_header->partition_entry_max);
+  fprintf(stdout,"gpt entry size: %d\n",g_header->partition_entry_size);
+}
+
+void hex_output(char* data,int size){
   int m=0;
   int i=0;
   int j=0;
@@ -65,6 +84,7 @@ void format_output(char* data,int size){
   }
 }
 
+
 int find_gpt_header_signature(FILE* fp){
   int i = 0,offset = 0, size = 512;
   uint64_t signature; // 8Byte
@@ -77,5 +97,54 @@ int find_gpt_header_signature(FILE* fp){
     }
   }
   return 0;
+}
 
+int read_partition_info(FILE* fp,GPT_HEADER *g_header ,PARTITION *partitions){
+  uint64_t partition_start_sector = g_header->partition_start;
+
+  // 将文件指针定位至分区表的开头
+  fseek(fp, 512 * partition_start_sector,SEEK_SET);
+  for (int i = 0; i < 128; i++) {
+    if (!fread(partitions + i, sizeof(PARTITION), 1, fp)) {
+      fprintf(stderr, "read partition error %d", fp);
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void output_partition(PARTITION *partitions) {
+  PARTITION *partition = NULL;
+
+  for (int i = 0 ; i < 128; i++ ) {
+    partition = partitions + i;
+
+    if (partition->partition_start_sector == 0) {
+      break;
+    }
+
+    fprintf(stdout,"partition type: ");
+    for (int j = 0; j < 16; j++){
+      fprintf(stdout," %02x ",*(partition->partition_id + j));
+    }
+    fprintf(stdout,"\n\rpartition id: ");
+    for (int j = 0; j < 16; j++){
+      fprintf(stdout," %02x ",*(partition->partition_type + j));
+    }
+    printf("\n");
+    fprintf(stdout,"partition start sector: %llu\n", partition->partition_start_sector);
+    fprintf(stdout,"partition end sector: %llu\n", partition->partition_end_sector);
+    fprintf(stdout,"partition type: 0x%llx\n", partition->partition_flag);
+  }
+}
+
+int read_first_partition(FILE* fp, PARTITION *partition){
+  // 定位文件指针至第一个扇区
+  fseek(fp,partition->partition_start_sector * 512,SEEK_SET);
+  char data[1024] = {0};
+  fread(data, 512,2 ,fp);
+
+  hex_output(data,1024);
+
+  return 0;
 }
--- diskRead.h
+++ diskRead.h
@@ -8,27 +8,45 @@
 
 #define GPT_HEADER_SIGNATURE 0x5452415020494645
 
-
+// GPT头
 typedef struct {
   uint64_t signature;
   int version;
   int header_size;
   uint32_t crc_header;
   int reserved;
-  uint64_t gpt_header_start;
-  uint64_t gpt_header_backup;
+  uint64_t efi_header_start;    // EFI信息区的起始扇区
+  uint64_t efi_header_backup;   // EFI信息区的备份扇区号
   uint64_t gpt_partition_start; // GPT分区区域的起始扇区号
   uint64_t gpt_partition_end;   // GPT分区区域的结束扇区号
   uint8_t  disk_guid[16];
-  uint64_t gpt_start;           // GPT分区表的起始扇区
-  uint32_t gpt_entry_count;     // GPT分区表总项数
-  uint32_t gpt_entry_size;      // GPT每个分区表占用的字节数
+  uint64_t partition_start;     // 分区表的起始扇区
+  uint32_t partition_entry_max; // 分区表总项数,最多可以有多少个分区表
+  uint32_t partition_entry_size;// 每个分区表占用的字节数
   uint32_t crc;
-  int reser[105];               // int 4 Byte
-} gpt_header;
+} GPT_HEADER;
 
-void format_output(char* data,int size);
+// GPT分区表
+typedef struct{
+  uint8_t  partition_type[16];
+  uint8_t  partition_id[16];
+  uint64_t partition_start_sector;
+  uint64_t partition_end_sector;
+  uint64_t partition_flag;
+  uint8_t partition_name[72];
+} PARTITION;
 
+// 辅助输出
+void hex_output(char* data,int size);
+
+// gpt头相关
 int find_gpt_header_signature(FILE* fp);
-int print_gpt_header(FILE* fp,gpt_header *gpt_header);
+int read_gpt_header(FILE* fp,GPT_HEADER *gpt_header);
+void output_gpt_header(GPT_HEADER *g_header);
 
+// gpt分区表相关
+int read_partition_info(FILE* fp,GPT_HEADER *g_header ,PARTITION *partition);
+void output_partition(PARTITION *partition);
+
+// 读取分区中的文件
+int read_first_partition(FILE* fp, PARTITION *partition);
